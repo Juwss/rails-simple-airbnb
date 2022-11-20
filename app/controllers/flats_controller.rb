587d6ca738338bd720b73d45d9b847668d1726cd
Julia Weiss <juliaweiss@live.fr>
2022-11-20 13:39:13 +0100

    part update ok

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -13,9 +13,23 @@ class FlatsController < ApplicationController
     redirect_to flat_path(@flat)
   end
 
+  def show
+    @flat = Flat.find(params[:id])
+  end
+
+  def edit
+    @flat = Flat.find(params[:id])
+  end
+
+  def update
+    @flat = Flat.find(params[:id])
+    @flat.update(flat_params)
+    redirect_to flat_path(@flat)
+  end
+
   private
 
   def flat_params
-    params.require(:flat).permit(:name, :address, :description, :number_of_guests, :price_per_night)
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
   end
 end
